0d267a5f2da6ed4b73f14d41a0805bdfc37c6730
sucystem <sucystem@outlook.com>
2020-12-01 04:24:28 +0900

    today schedule

--- src/pages/MainPage/components/main/index.js
+++ src/pages/MainPage/components/main/index.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react'
 import './main.css'
 import { useHistory } from 'react-router-dom';
+import callAPI from '../../../../_utils/apiCaller'
 
 class Main extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            schedule: []
+        }
+    }
+    
+    getToken = () => {
+        const token = localStorage.getItem('token');
+        return {
+            auth_token: token,
+        }
+    }
+
+    getTodaySchedule = async () => {
+        const res = await callAPI(
+            `schedule/today`, 
+            'GET', 
+            { ...this.getToken() }, 
+            null,
+        );
+
+        if(res.data.result === 'true') {
+            this.setState({
+                schedule: res.data.data
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.getTodaySchedule();
     }
 
     render() {
@@ -24,9 +55,11 @@ class Main extends Component {
                 <div class="box_head">오늘의 일정</div>
                 <div>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        {this.state.schedule.map((schedule) => {
+                            return (
+                                <li>{schedule.title}</li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
