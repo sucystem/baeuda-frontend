9035ffd797ff305572f7f5c07003cb6b53403e9e
hyemdooly <hyemdooly@gmail.com>
2020-12-01 02:57:32 +0900

    schedule 진행중

--- src/pages/MySchedulePage/components/MyTimeTable/index.js
+++ src/pages/MySchedulePage/components/MyTimeTable/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { createRef } from 'react';
 
 import TuiCalendar from 'tui-calendar';
 
@@ -6,39 +6,87 @@ import 'tui-calendar/dist/tui-calendar.css';
 
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
+import callAPI from '../../../../_utils/apiCaller';
+const moment = require('moment');
 
-function MyTimeTable() {
+class MyTimeTable extends React.Component {
+    state = {
+        month: moment().format('MM'),
+        calendar: null, 
+        datas: [],
+    };
 
-    const calendarRef = useRef(null);
+    constructor(props) {
+        super(props);
+        // https://ko.reactjs.org/docs/refs-and-the-dom.html
+        this.calendarRef = createRef(); 
+    }
 
-    const [calendar, setCalendar] = useState(null);
+    getToken = () => {
+        const token = localStorage.getItem('token');
+        return {
+            auth_token: token,
+        }
+    }
 
-    useEffect(() => {
-        const newCalendar = new TuiCalendar(calendarRef.current, {
-            defaultView: 'week',
-            taskView: false,
+    getDatas = async () => {
+        console.log(`schedule/${this.state.month}`);
+        const res = await callAPI(
+            `schedule/${this.state.month}`, 
+            'GET', 
+            { ...this.getToken() }, 
+            null,
+        );
+        
+        if (res.data.result === 'true') {
+            console.log(res.data);
+            this.setState({ datas: res.data.data });
+            return;
+        }
+
+        alert(res.data.msg);
+    };
+
+    componentDidMount() {
+        const newCalendar = new TuiCalendar(this.calendarRef.current, { 
+            defaultView: 'month',
+            taskView: true,
         });
 
-        newCalendar.createSchedules([
+        // TODO 여기서 서버 데이터 받아오기
+        this.getDatas();
+
+        newCalendar.createSchedules([ // test용
             {
                 id: '1',
                 calendarId: '1',
-                title: '이준환 입소!!',
+                title: '이준환 입소',
                 category: 'time',
                 dueDateClass: '',
-                start: '2020-12-01T09:30:00+09:00',
-                end: '2020-12-01T09:30:00+09:00'
+                start: '2020-12-01T22:30:00+09:00',
+                end: '2020-12-03T02:30:00+09:00'
             },
+            {
+                id: '2',
+                calendarId: '2',
+                title: '이준환 입소?!',
+                category: 'time',
+                dueDateClass: '',
+                start: '2020-12-05T22:30:00+09:00',
+                end: '2020-12-07T02:30:00+09:00'
+            }
         ]);
 
-        setCalendar(newCalendar);
-    }, []);
+        this.setState({ calendar: newCalendar });
+    }
 
-    return (
-        <div>
-            <div ref={calendarRef}></div>
-        </div>
-    );
-}
+    render() {
+        return (
+            <div className='MyCalendar'>
+                <div ref={this.calendarRef}></div>
+            </div>
+        );
+    }
+};
 
 export default MyTimeTable;
