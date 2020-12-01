2b4c9907924a1a4f22639bf3913a3443bed78024
ksi0678 <ksi0678@naver.com>
2020-12-02 02:00:45 +0900

    study 1차

--- src/pages/StudyPage/components/StudyList/index.js
+++ src/pages/StudyPage/components/StudyList/index.js
@@ -1,20 +1,67 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './style.css'
 import { useHistory } from 'react-router-dom';
+import callAPI from '../../../../_utils/apiCaller';
+const moment = require('moment');
 
-function StudyList(){
-    let history = useHistory();
-    return <div id="StudyList">
+class StudyList extends Component{
+    constructor(props) {
+        super(props);
+        if (!(localStorage.getItem('token') && localStorage.getItem('user'))) {
+            this.props.history.push('/')
+        }
+
+        this.state = {
+            posts: []
+        }
+
+    }
+
+    getToken = () => {
+        const token = localStorage.getItem('token');
+        return {
+            auth_token: token,
+        }
+    }
+
+    getPosts = async() => {
+        const boardId = this.props.match.params.board_id;
+        callAPI(`study/showList`, 'GET', {...this.getToken()}, null).then(res => {
+            if(res.data.result === 'true'){
+                this.setState ({
+                    posts: res.data.data
+                })
+            } else {
+                alert(res.data.msg)
+            }
+            
+        });
+    }
+
+    componentDidMount() {
+        this.getPosts();
+    }
+
+    render(){
+        let history = this.props.history;
+        return(<div id="StudyList">
         <div id="studyRoom">
-            <div class="study_room_box">
-                <div class="study_box_name">
-                    소프트웨어 공학개론 스터디
-                </div>
-                <div class="study_box_button_enter" onClick={() => history.push("/study/StudyRoom") }>입장</div>
-                <div class="study_box_button_exit" >탈퇴하기</div>
-            </div>
+                {
+                    this.state.posts.map((item) => {
+                        return (
+                            <div class="study_room_box">
+                                <div class="study_box_name">
+                                    {item.name}
+                                </div>
+                                <div class="study_box_button_enter" onClick={() => history.push("/study/StudyRoom/" + item.id) }>입장</div>
+                                <div class="study_box_button_exit"  onClick={() => history.push("/study/StudyRoomExit/" + item.id) }>탈퇴하기</div>
+                            </div>
+                        )
+                    })
+                }
         </div>
-    </div>
+    </div>)
+    }
 
 }
 
--- src/pages/StudyPage/components/StudyMain/index.js
+++ src/pages/StudyPage/components/StudyMain/index.js
@@ -1,10 +1,49 @@
 import React, {Component} from 'react'
 import './style.css'
 import { useHistory } from 'react-router-dom';
+import callAPI from '../../../../_utils/apiCaller';
+const moment = require('moment');
 
 class Main extends Component {
     constructor(props) {
         super(props);
+
+        if (!(localStorage.getItem('token') && localStorage.getItem('user'))) {
+            this.props.history.push('/')
+        }
+
+        this.state = {
+            study_list: [],
+            study_schedule : [],
+            study_recruit : []
+        }
+    }
+
+    getToken = () => {
+        const token = localStorage.getItem('token');
+        return {
+            auth_token: token,
+        }
+    }
+
+    getPosts = async() => {
+        const boardId = this.props.match.params.board_id;
+        callAPI(`study/main`, 'GET', {...this.getToken()}, null).then(res => {
+            if(res.data.result === 'true'){
+                this.setState ({
+                    study_list : res.data.data.row1,
+                    study_schedule : res.data.data.row2,
+                    study_recruit : res.data.data.row3
+                })
+            } else {
+                alert(res.data.msg)
+            }
+            
+        });
+    }
+
+    componentDidMount() {
+        this.getPosts();
     }
 
     render() {
@@ -14,29 +53,47 @@ class Main extends Component {
                 <div class="box_head" onClick={() => history.push("/StudyList") }>스터디 목록</div>
                 <div>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        {   
+                            this.state.study_list.map((item) => {
+                                return (
+                                    <li key={item.id} onClick={() => history.push("/studyRoom/" + item.id)}>
+                                        <div>{item.name}</div>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </div>
             <div id="studySchadule" class="menu_box">
-                <div class="box_head" onClick={() => history.push("/StudySchedule") }>스터디 일정</div>
+                <div class="box_head" onClick={() => history.push("/study/StudySchedule") }>스터디 일정</div>
                 <div>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        {   
+                            this.state.study_schedule.map((item) => {
+                                return (
+                                    <li key={item.id} onClick={() => history.push("/schedule/" + item.id)}>
+                                        <div>{item.name}</div>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </div>
             <div id="studyRecruit" class="menu_box">
-                <div class="box_head" onClick={() => history.push("/StudyRecruit") }>스터디 모집</div>
+                <div class="box_head" onClick={() => history.push("/study/StudyRecruit") }>스터디 모집</div>
                 <div>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        {   
+                            this.state.study_recruit.map((item) => {
+                                return (
+                                    <li key={item.id} onClick={() => history.push("/studyRoom/" + item.id)}>
+                                        <div>{item.name}</div>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </div>
