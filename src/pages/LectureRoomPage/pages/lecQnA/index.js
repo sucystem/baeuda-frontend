f21e85200f8f9a13fa314cb90e7a7aef5d0e168e
sucystem <sucystem@outlook.com>
2020-12-01 14:09:46 +0900

    lecture room

--- src/pages/LectureRoomPage/pages/lecQnA/index.js
+++ src/pages/LectureRoomPage/pages/lecQnA/index.js
@@ -1,29 +1,84 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './lecQnA.css'
-import { useHistory } from 'react-router-dom';
+import callAPI from '../../../../_utils/apiCaller'
+const moment = require('moment')
 
-function LectureQnA(){
-    let history = useHistory();
-    return <div id="lecQnA">
-        <div id="SubjectName">소프트웨어공학개론</div>
-        <div id="SubjectQnA">
-            <ul>
-                <li>
-                    <div class="Name TH">글 제목</div>
-                    <div class="Writer TH">작성자</div>
-                    <div class="Hits TH">조회수</div>
-                    <div class="Date TH">작성일</div>
-                </li>
-                <li >
-                    <div class="Name">싱글톤 패턴 관련 질문</div>
-                    <div class="Writer">양보승</div>
-                    <div class="Hits">21</div>
-                    <div class="Date">2020.11.02 11:50:48</div>
-                </li>
-            </ul>
+class LectureQnA extends Component {
+    constructor(props){
+        super(props);
+
+        const { lecture_id } = this.props.match.params;
+        this.state = {
+            lecture_id: lecture_id,
+            lecture: [],
+            posts: []
+        }
+    }
+
+    getToken = () => {
+        const token = localStorage.getItem('token');
+        return {
+            auth_token: token,
+        }
+    }
+
+    getLecture = async () => {
+        const lecture_id = this.state.lecture_id;
+        callAPI(`lecture/info/${lecture_id}`, 'GET', { ...this.getToken() }, null).then(res => {
+            if (res.data.result === 'true') {
+                this.setState({
+                    lecture: res.data.data
+                });
+            } else {
+                alert(res.data.msg);
+            }
+        })
+    }
+
+    getNotices = async () => {
+        const lecture_id = this.state.lecture_id;
+        callAPI(`lecture/room/qnas/${lecture_id}`, 'GET', { ...this.getToken() }, null).then(res => {
+            if (res.data.result === 'true') {
+                this.setState({
+                    posts: res.data.data
+                })
+            } else {
+                alert(res.data.msg);
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.getLecture();
+        this.getNotices();
+    }
+
+    render() {
+        let history = this.props.history;
+        return <div id="lecNotice">
+            <div id="SubjectName">{this.state.lecture.name}</div>
+            <div id="SubjectNotice">
+                <ul>
+                    <li>
+                        <div class="Name TH">글 제목</div>
+                        <div class="Writer TH">작성자</div>
+                        <div class="Hits TH">조회수</div>
+                        <div class="Date TH">작성일</div>
+                    </li>
+                    {this.state.posts.map((post, i) => {
+                        return (
+                            <li onClick= {() => history.push(`/lectureroom/${this.state.lecture_id}/${this.state.lecture_id}/post/${post.id}`)}>
+                                <div class="Name">{post.title}</div>
+                                <div class="Writer">{post.user_name}</div>
+                                <div class="Hits">{post.count}</div>
+                                <div class="Date">{moment(post.regDate).format("YYYY-MM-DD hh:mm:ss")}</div>
+                            </li>
+                        );
+                    })}
+                </ul>
         </div>
-    </div>
-
+        </div>
+    }
 }
 
 export default LectureQnA;
