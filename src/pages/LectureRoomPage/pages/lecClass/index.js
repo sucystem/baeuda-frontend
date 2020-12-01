2cc04ff40ed8f1f9fb0d905d8e2209b5c5224ae4
sucystem <sucystem@outlook.com>
2020-12-01 12:05:48 +0900

    lessons

--- src/pages/LectureRoomPage/pages/lecClass/index.js
+++ src/pages/LectureRoomPage/pages/lecClass/index.js
@@ -6,11 +6,14 @@ class LectureClass extends Component {
     constructor(props) {
         super(props);
 
+        const { lecture_id } = this.props.match.params;
         this.state = {
-            lecture: []
+            lecture_id: lecture_id,
+            lecture: [],
+            lessons: []
         }
     }
-    
+
     getToken = () => {
         const token = localStorage.getItem('token');
         return {
@@ -19,9 +22,9 @@ class LectureClass extends Component {
     }
 
     getLecture = async () => {
-        const {lecture_id} = this.props.match.params;
-        callAPI(`lecture/${lecture_id}/info`, 'GET', {...this.getToken()}, null).then(res => {
-            if(res.data.result === 'true') {
+        const lecture_id = this.state.lecture_id;
+        callAPI(`lecture/${lecture_id}/info`, 'GET', { ...this.getToken() }, null).then(res => {
+            if (res.data.result === 'true') {
                 this.setState({
                     lecture: res.data.data
                 });
@@ -31,8 +34,22 @@ class LectureClass extends Component {
         })
     }
 
+    getLessons = async () => {
+        const lecture_id = this.state.lecture_id;
+        callAPI(`lecture/${lecture_id}/lessons`, 'GET', { ...this.getToken() }, null).then(res => {
+            if (res.data.result === 'true') {
+                this.setState({
+                    lessons: res.data.data
+                });
+            } else {
+                alert(res.data.msg);
+            }
+        })
+    }
+
     componentDidMount() {
         this.getLecture();
+        this.getLessons();
     }
 
     render() {
@@ -41,12 +58,17 @@ class LectureClass extends Component {
             <div id="SubjectName">{this.state.lecture.name}</div>
             <div id="SubjectProgression">
                 <ul>
-                    <li class="classInfo">
-                        <div class="classIndex">1강</div>
-                        <div class="ProgressionBar"></div>
-                        <div class="ProgressionPercent">100%</div>
-                        <div class="TakeCourse">수강하기</div>
-                    </li>
+                    {this.state.lessons.map((lesson, i) => {
+                        return (
+                            <li class="classInfo">
+                                <div class="classIndex">{i+1}강</div>
+                                <div class="ProgressionBar"></div>
+                                <div class="ProgressionPercent">{lesson.progress}%</div>
+                                <a href={lesson.link} target='_blank'><div class="TakeCourse">수강하기</div></a>
+                            </li>
+                        );
+                    })
+                    }
                 </ul>
             </div>
         </div>
